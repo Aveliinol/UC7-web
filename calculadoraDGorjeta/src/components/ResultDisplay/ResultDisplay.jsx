67c4559cf2ff9a616a8db17f90835566e177ad35
Lucas <lugabriell084@gmail.com>
2025-03-25 17:18:33 -0300

    feat: inicializei o aplicativo de calculadora de gorjetas com React e Vite, incluindo componentes e estilos

---
+++ calculadoraDGorjeta/src/components/ResultDisplay/ResultDisplay.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ResultDisplay({gorjeta, total}){
+    return(
+        <>
+          <div>
+            <p>Gorjeta: {gorjeta}</p>
+            <p>Total: {total}</p>
+        </div>
+        </>
+    )
+}
+
+export default ResultDisplay
